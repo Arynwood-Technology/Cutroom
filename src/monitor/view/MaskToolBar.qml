D-Ogi/kdenlive

Roto brush UX: brush strokes, live preview, overlay modes, undo, corrections
Brush tool as default input for SAM2 mask creation. Canvas-based
stroke drawing with 15px sampling, Alt+drag to exclude. 300ms debounce
for preview generation. Three overlay modes (color, boundary, alpha).
Full undo/redo stack (Ctrl+Z / Ctrl+Shift+Z) for points, strokes,
boxes, and moves. Streaming per-frame propagation progress. Correction
re-propagation via rerender= with reset_state.

Edge-case hardening:
- Bounds check in moveMonitorControlPoint (crash on invalid index)
- Buffered stdout parsing from SAM process (partial line glitch)
- Stop debounce timer on generateMask (spurious preview during render)
- Reset brush state on maskMode change mid-stroke
- Clear brush canvas on frame seek
- EOF guard on Python stdin (infinite CPU spin on parent crash)
- reset_state on render= (duplicate keyframes on double render)
- Empty mask guard in predict() (IndexError on no detection)
- Global predictor restoration after render (silent recreation)
- Alpha channel mode uses mask as alpha (opaque black on empty mask)

Pre-existing fixes:
- Box serialization round-trip (QRect constructor mismatch)
- Box-only frames missing from keyframe ruler
- Debounce timer not stopped in cleanup/abort/terminate paths

src/monitor/view/MaskToolBar.qml
@@ -53,6 +53,17 @@ MouseArea {
         Column {
             width: parent.width
             Kdenlive.MonitorToolButton {
+                id: brushToolButton
+                objectName: "brushTool"
+                iconName: "draw-brush"
+                toolTipText: root.brushMode ? i18n("Brush Tool (Active) — Click to switch to Point mode") : i18n("Point Tool (Active) — Click to switch to Brush mode")
+                checked: root.brushMode
+                visible: root.maskMode != Kdenlive.MaskModeType.MaskPreview
+                onClicked: {
+                    root.brushMode = !root.brushMode
+                }
+            }
+            Kdenlive.MonitorToolButton {
                 id: fullscreenButton
                 objectName: "fullScreen"
                 iconName: "view-fullscreen"
@@ -64,7 +75,7 @@ MouseArea {
                 iconName: "media-record"
                 toolTipText: i18n("Generate Mask")
                 checkable: false
-                visible: root.maskMode != MaskModeType.MaskPreview
+                visible: root.maskMode != Kdenlive.MaskModeType.MaskPreview
                 onClicked: {
                     generateLabel.visible = true
                     if (root.keyframes.length > 0 || (root.boxCoords[2] > 0 && root.boxCoords[3] > 0)) {
@@ -90,6 +101,18 @@ MouseArea {
                 }
             }
             Kdenlive.MonitorToolButton {
+                objectName: "overlayMode"
+                iconName: controller.maskOverlayMode === 0 ? "color-fill" : controller.maskOverlayMode === 1 ? "draw-polyline" : "visibility"
+                toolTipText: {
+                    if (controller.maskOverlayMode === 0) return i18n("Overlay: Color (click to switch to Boundary)")
+                    if (controller.maskOverlayMode === 1) return i18n("Overlay: Boundary (click to switch to Alpha)")
+                    return i18n("Overlay: Alpha Channel (click to switch to Color)")
+                }
+                onClicked: {
+                    controller.maskOverlayMode = (controller.maskOverlayMode + 1) % 3
+                }
+            }
+            Kdenlive.MonitorToolButton {
                 objectName: "invertMask"
                 iconName: "edit-select-invert"
                 toolTipText: i18n("Invert Mask")
@@ -100,7 +123,7 @@ MouseArea {
             Kdenlive.MonitorToolButton {
                 objectName: "abortMask"
                 iconName: "dialog-close"
-                toolTipText: root.maskMode != MaskModeType.MaskPreview ? i18n("Exit Mask Creation") : i18n("Exit Preview Mode")
+                toolTipText: root.maskMode != Kdenlive.MaskModeType.MaskPreview ? i18n("Exit Mask Creation") : i18n("Exit Preview Mode")
                 checkable: false
                 onClicked: {
                     root.exitMaskPreview()

Arynwood-Technology/Cutroom

src/monitor/view/MaskToolBar.qml
@@ -58,6 +58,17 @@ MouseArea {
         Column {
             width: parent.width
             K.MonitorToolButton {
+                id: brushToolButton
+                objectName: "brushTool"
+                iconName: "draw-brush"
+                toolTipText: root.brushMode ? KI18n.i18n("Brush Tool (Active) — Click to switch to Point mode") : KI18n.i18n("Point Tool (Active) — Click to switch to Brush mode")
+                checked: root.brushMode
+                visible: root.maskMode != MaskModeType.MaskPreview
+                onClicked: {
+                    root.brushMode = !root.brushMode
+                }
+            }
+            K.MonitorToolButton {
                 id: fullscreenButton
                 objectName: "fullScreen"
                 iconName: "view-fullscreen"
@@ -92,6 +103,18 @@ MouseArea {
                     } else {
                         barZone.monitorController.maskOpacity = barZone.monitorController.maskOpacity * 2;
                     }
+                }
+            }
+            K.MonitorToolButton {
+                objectName: "overlayMode"
+                iconName: barZone.monitorController.maskOverlayMode === 0 ? "color-fill" : barZone.monitorController.maskOverlayMode === 1 ? "draw-polyline" : "visibility"
+                toolTipText: {
+                    if (barZone.monitorController.maskOverlayMode === 0) return KI18n.i18n("Overlay: Color (click to switch to Boundary)")
+                    if (barZone.monitorController.maskOverlayMode === 1) return KI18n.i18n("Overlay: Boundary (click to switch to Alpha)")
+                    return KI18n.i18n("Overlay: Alpha Channel (click to switch to Color)")
+                }
+                onClicked: {
+                    barZone.monitorController.maskOverlayMode = (barZone.monitorController.maskOverlayMode + 1) % 3
                 }
             }
             K.MonitorToolButton {